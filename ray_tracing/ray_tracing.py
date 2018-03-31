09b487b5b4e8f699ccf49f606bbbf25ae07d38cc
ItaloBruno <italobruno_207@hotmail.com>
2018-03-30 21:21:28 -0300

    Conclusão dos casos ortográfico e oblíquo

--- ray_tracing/ray_tracing.py
+++ ray_tracing/ray_tracing.py
@@ -1,16 +1,15 @@
 import numpy
 from math import pow, sqrt
-#import copy
 
-# Parâmetros disponibilizados
-left   = -10
-right  =  10
-top    =  5
-bottom = -5
+# Valores dos lados do plano da imagem
+left      = -10
+right     =  10
+top       =  5
+bottom    = -5
+distancia =  4
 
 # Declaração de vetores que serão usados
 vetorE = [2, -2, 3]
-
 vetorW = [0, 0, 0]
 vetorT = [0, 0, 0]
 vetorV = [0, 0, 0]
@@ -26,22 +25,33 @@ def criaMatriz(numlinhas, numColunas, valor):
     for x in range(numColunas):
         linha = []
         for y in range(numlinhas):
-	        linha += [valor]
-        matriz += [linha]
+	        linha.append(valor)
+        matriz.append(linha)
     return matriz
 
-def normaVetor(vetor):
+def calculaNormaVetor(vetor):
     norma = pow(vetor[0], 2) + pow(vetor[1], 2) + pow(vetor[2], 2)
     norma = sqrt(norma)
     return norma
 
-def vetorUnitario(vetor, norma):
+def calculaVetorUnitario(vetor, norma):
     vetorAux = vetor[:]
     for i in range (len(vetorAux)):
         vetorAux[i] = vetorAux[i]/norma
     return vetorAux
 
-def calculoVetorT(vetor):
+# def produtoEscalar(vetor, escalar):
+#     vetorAux = vetor[:]
+#     for i in range(len(vetorAux)):
+#         vetorAux[i] = vetorAux[i] * escalar
+#     return vetorAux
+
+def calculaVetorW(vetorE):
+    normaVetorE = calculaNormaVetor(vetorE)
+    vetor = calculaVetorUnitario(vetorE, normaVetorE)
+    return vetor
+
+def calculaVetorT(vetor):
     vetorAux  = vetor[:]
     indiceMin = 0
     for indice, valor in enumerate(vetor):
@@ -50,49 +60,77 @@ def calculoVetorT(vetor):
     vetorAux[indiceMin] = 1
     return vetorAux
 
-def produtoEscalar(vetor, escalar):
-    vetorAux = vetor[:]
-    for i in range(len(vetorAux)):
-        vetorAux[i] = vetorAux[i] * escalar
-    return vetorAux
+def calculaVetorU(vetorT, vetorW):
+    prodVetorial = numpy.cross(vetorT, vetorW)
+    norma        = calculaNormaVetor(prodVetorial)
+    vetorU       = prodVetorial / norma
+    return vetorU
 
-def calculaValoresUV(matrizImagem, numColunas, numlinhas, left, right, top, bottom):
+def calculaVetorV(vetorW, vetorU):
+    vetorV = numpy.cross(vetorW, vetorU)
+    return vetorV
+
+def calculaValoresUV(matrizImagem, nX, nY, l, r, t, b):
+    matrizAux = matrizImagem[:]
     for i in range(numColunas):
         for j in range(numlinhas):
-            u = float((left + (right -left) * (i + 0.5))) / numColunas
-            v = float((bottom + (top - bottom) * (j + 0.5))) / numlinhas
-            matrizImagem[i, j] = (u, v)
+            u = float(l + (r - l) * (i + 0.5)) / nX
+            v = float(b + (t - b) * (j + 0.5)) / nY
+            matrizAux[i][j] = (u, v)
+    return matrizAux
 
-def casoOrtografico(vetorE, vetorU, vetorV, vetorW):
-    u = -1
-    v = -1
-    direcao  = produtoEscalar(vetorW, -1)
-    escalar1 = produtoEscalar(vetorU, u)
-    escalar2 = produtoEscalar(vetorV, v)
-    origem   = vetorE + escalar1 + escalar2
+def casoOrtografico(matrizImagem, nX, nY, vetE, vetU, vetV, vetW):
+    direcaoRaio = numpy.dot(vetW, -1)
+    origemRaio  = 0
+    for i in range(nX):
+        for j in range(nY):
+            for k in range(0, 1):
+                if(k%2 == 0):
+                    escalar1    = vetU * matrizImagem[i][j][k]
+                    escalar2    = vetV * matrizImagem[i][j][k+1]
+                    origemRaio  = vetE + escalar1 + escalar2
+                    matrizImagem[i][j] = (direcaoRaio, origemRaio)
+
+
+def casoObliquo(matrizImagem, nX, nY, vetE, vetU, vetV, vetW, distancia):
+    # origem do raio = vetor E
+    origemRaio  = vetorE[:]
+    direcaoRaio = 0
+    for i in range(nX):
+        for j in range(nY):
+           for k in range(0,1):
+                if(k%2 ==0):
+                    # direção do raio = (-d * vetorW) + (coordenada U * vetor U) + (coordenada V * vetor V)
+                    escalar1    = numpy.dot(vetW, -distancia)
+                    escalar2    = vetU * matrizImagem[i][j][k]
+                    escalar3    = vetV * matrizImagem[i][j][k+1]
+                    direcaoRaio = escalar1 + escalar2 + escalar3
+                    matrizImagem[i][j] = (direcaoRaio, origemRaio)
 
 #===========================================================================
-
-# # Inicia todos os elementos da matriz imagem com valor 0
-# matrizImagem = criaMatriz(numlinhas, numColunas, 0)
+# Inicia todos os elementos da matriz imagem com valor 0
+matrizImagem = criaMatriz(numlinhas, numColunas, 0)
 
 # Calculo do vetor W, que será o vetor unitário do vetor E
-normaVetorE = normaVetor(vetorE)
-vetorW = vetorUnitario(vetorE, normaVetorE)
+vetorW = calculaVetorW(vetorE)
 
 # O vetor T terá os mesmos valores do vetor W, 
 # com a diferença que o menor elemento (valor absoluto) de T será setado para 1
-vetorT = calculoVetorT(vetorW)
+vetorT = calculaVetorT(vetorW)
 
 # Vetor U será o vetor unitário do produto vetorial entre os vetores T e W
-vetorialTxW = numpy.cross(vetorT, vetorW)
-normaTxW    = normaVetor(vetorialTxW)
-vetorU      = vetorUnitario(vetorialTxW, normaTxW)
+vetorU = calculaVetorU(vetorT, vetorW)
 
 # Vetor V será o resultado do produto vetorial entre os vetores W e U
-vetorV = numpy.cross(vetorW, vetorU)
+vetorV = calculaVetorV(vetorW, vetorU)
 
-print(vetorU)
-print(vetorV)
-print(vetorW)
+# print(vetorU)
+# print(vetorV)
+# print(vetorW)
 
+matrizImagem = calculaValoresUV(matrizImagem, numColunas, numlinhas, left, right, top, bottom)
+
+casoOrtografico(matrizImagem, numColunas, numlinhas, vetorE, vetorU, vetorV, vetorW)
+
+# casoObliquo(matrizImagem, numColunas, numlinhas, vetorE, vetorU, vetorV, vetorW, 4)
+print(matrizImagem)
