35149892a4f38c4c0f5fb446a0f336b44d065266
ItaloBruno <italobruno_207@hotmail.com>
2018-04-19 23:29:57 -0300

    Cálculo do delta incompleto e função de criação da imagem funfando

--- ray_tracing/imagem.py
+++ ray_tracing/imagem.py
@@ -1,15 +1,18 @@
-class Imagem:
-    def __init__(self, numColunas, numLinhas)
-        self.numColunas = numColunas
-        self.numLinhas  = numLinhas
+from PIL import Image as PilImage
 
-    # def criaMatriz(self):
-    #     matriz = []
-    #     for x in range(self.numColunas):
-    #         linha = []
-    #         for y in range(self.numLinhas):
-    #             linha.append(0)
-    #         matriz.append(linha)
-        
-    #     self.matriz = matriz 
+class Imagem:
+    def __init__(self, num_colunas, num_linhas):
+        self.num_colunas = num_colunas
+        self.num_linhas  = num_linhas
+
+    def Cria_Imagem(matriz_imagem):
+        imagem = PilImage.new('RGB', (self.num_colunas, self.num_lihas))
+        imagem = imagem.load()
+        for x in range(self.num_colunas):
+            for y in range(self.num_linhas):
+                imagem[x, y] = (matriz_imagem[x][y][0],
+                                matriz_imagem[x][y][1], 
+                                matriz_imagem[x][y][2])
+        imagem.show()
+        imagem.save("resultado.jpg")
 
--- ray_tracing/ray_tracing.py
+++ ray_tracing/ray_tracing.py
@@ -1,191 +1,159 @@
 import numpy
-from math import pow, sqrt
-import PIL
+from PIL import Image as PilImage
 
-# Valores dos lados do plano da imagem
-left      = -10
-right     =  10
-top       =  5
-bottom    = -5
-distancia =  4
+def Norma_Vetor(vetor):
+    norma = 0
+    for indice in range(len(vetor)):
+        norma = norma + vetor[indice] ** 2
+    return norma ** (1/2)
 
-# Declaração de vetores que serão usados
-vetorE = [2, -2, 3]
-vetorW = [0, 0, 0]
-vetorT = [0, 0, 0]
-vetorV = [0, 0, 0]
-vetorU = [0, 0, 0]
+def Calculo_Vetor_Unitario(vetor):
+    vetor_unitario = vetor[:]
+    norma_vetor = Norma_Vetor(vetor)
+    for indice in range(len(vetor)):
+        vetor_unitario[indice] = vetor[indice] / norma_vetor
+    return vetor_unitario
+    
+def Calculo_Vetor_W(vet_a):
+    vet_w = Calculo_Vetor_Unitario(vet_a)
+    return vet_w
 
-# Dimensões da imagem
-numColunas = 640
-numlinhas  = 480
+def Calculo_Vetor_T(vet_w):
+    vet_t = vet_w[:]
+    indice_menor_valor = 0
+    for indice, valor in enumerate(vet_w):
+        if(0 > abs(valor)):
+            indice_menor_valor = indice
+    vet_t[indice_menor_valor] = 0
+    return vet_t
 
-# ================================ Funções ================================
-def criaMatriz(numlinhas, numColunas, valor):
+def Calculo_Vetor_U(vet_t, vet_w):
+    vet_u = numpy.cross(vet_t, vet_w)
+    # print(vet_u)
+    vet_u = Calculo_Vetor_Unitario(vet_u)
+    return vet_u
+
+def Calculo_Vetor_V(vet_w, vet_u):
+    vet_v = numpy.cross(vet_w, vet_u)
+    return vet_v
+
+def Criar_Matriz(colunas, linhas, valor):
     matriz = [] 
-    for x in range(numColunas):
+    for x in range(colunas):
         linha = []
-        for y in range(numlinhas):
+        for y in range(linhas):
 	        linha.append(valor)
         matriz.append(linha)
     return matriz
 
-def calculaNormaVetor(vetor):
-    norma = pow(vetor[0], 2) + pow(vetor[1], 2) + pow(vetor[2], 2)
-    norma = sqrt(norma)
-    return norma
+def Calculo_Valore_U_V(matriz, colunas, linhas, l, r, b, t):
+    for i in range(colunas):
+        for j in range(linhas):
+            u = (l + (r - l) * (i + 0.5))/ linhas
+            v = (b + (t - b) * (j + 0.5))/ colunas
+            matriz[i][j] = (u, v)
+    
+def Caso_Ortografico(matriz_valores_uv, colunas, linhas, vet_a, vet_w, vet_u, vet_v):
+    matriz_result = matriz_valores_uv[:]
+    direcao_raio  = numpy.dot(vet_w, -1)
+    for x in range(colunas):
+        for y in range(linhas):
+            comp_u = numpy.dot(vet_u, matriz_valores_uv[x][y][0])
+            comp_v = numpy.dot(vet_v, matriz_valores_uv[x][y][1])
+            origem_raio = vet_a + comp_u + comp_v
+            # print(direcao_raio)
+            # print(origem_raio)
+            matriz_result[x][y] = (direcao_raio, origem_raio)
+            print(matriz_result[x][y])
+    return matriz_result
 
-def calculaVetorUnitario(vetor, norma):
-    vetorAux = vetor[:]
-    for i in range (len(vetorAux)):
-        vetorAux[i] = vetorAux[i]/norma
-    return vetorAux
-
-# def produtoEscalar(vetor, escalar):
-#     vetorAux = vetor[:]
-#     for i in range(len(vetorAux)):
-#         vetorAux[i] = vetorAux[i] * escalar
-#     return vetorAux
-
-def calculaVetorW(vetorE):
-    normaVetorE = calculaNormaVetor(vetorE)
-    vetor = calculaVetorUnitario(vetorE, normaVetorE)
-    return vetor
-
-def calculaVetorT(vetor):
-    vetorAux  = vetor[:]
-    indiceMin = 0
-    for indice, valor in enumerate(vetor):
-        if(0 > abs(valor)):
-            indiceMin = indice
-    vetorAux[indiceMin] = 1
-    return vetorAux
-
-def calculaVetorU(vetorT, vetorW):
-    prodVetorial = numpy.cross(vetorT, vetorW)
-    norma        = calculaNormaVetor(prodVetorial)
-    vetorU       = prodVetorial / norma
-    return vetorU
-
-def calculaVetorV(vetorW, vetorU):
-    vetorV = numpy.cross(vetorW, vetorU)
-    return vetorV
-
-def calculaValoresUV(matrizImagem, nX, nY, l, r, t, b):
-    matrizAux = matrizImagem[:]
-    for i in range(numColunas):
-        for j in range(numlinhas):
-            u = float(l + (r - l) * (i + 0.5)) / nX
-            v = float(b + (t - b) * (j + 0.5)) / nY
-            matrizAux[i][j] = (u, v)
-    return matrizAux
-
-def casoOrtografico(matrizImagem, nX, nY, vetE, vetU, vetV, vetW):
-    direcaoRaio = numpy.dot(vetW, -1)
-    origemRaio  = 0
-    for i in range(nX):
-        for j in range(nY):
-            for k in range(0, 1):
-                if(k%2 == 0):
-                    escalar1    = vetU * matrizImagem[i][j][k]
-                    escalar2    = vetV * matrizImagem[i][j][k+1]
-                    origemRaio  = vetE + escalar1 + escalar2
-                    matrizImagem[i][j] = (direcaoRaio, origemRaio)
+def Caso_Obliquo(matriz_valores_uv, colunas, linhas, vet_a, vet_w, vet_u, vet_v, dist):
+    matriz_result = matriz_valores_uv[:]
+    origem_raio   = vet_a[:]
+    for x in range(colunas):
+        for y in range(linhas):
+            aux    = numpy.dot(vet_w, -dist)
+            comp_u = numpy.dot(vet_u, matriz_valores_uv[x][y][0])
+            comp_v = numpy.dot(vet_v, matriz_valores_uv[x][y][1])
+            direcao_raio =  aux + comp_u + comp_v
+            matriz_result[x][y] = (direcao_raio, origem_raio)
+            # print(matriz_result[x][y])
+    return matriz_result
 
 
-def casoObliquo(matrizImagem, nX, nY, vetE, vetU, vetV, vetW, distancia):
-    # origem do raio = vetor E
-    origemRaio  = vetorE[:]
-    direcaoRaio = 0
-    for i in range(nX):
-        for j in range(nY):
-           for k in range(0,1):
-                if(k%2 ==0):
-                    # direção do raio = (-d * vetorW) + (coordenada U * vetor U) + (coordenada V * vetor V)
-                    escalar1    = numpy.dot(vetW, -distancia)
-                    escalar2    = vetU * matrizImagem[i][j][k]
-                    escalar3    = vetV * matrizImagem[i][j][k+1]
-                    direcaoRaio = escalar1 + escalar2 + escalar3
-                    matrizImagem[i][j] = (direcaoRaio, origemRaio)
-
-#===========================================================================
-# Inicia todos os elementos da matriz imagem com valor 0
-matrizImagem = criaMatriz(numlinhas, numColunas, 0)
-
-# Calculo do vetor W, que será o vetor unitário do vetor E
-vetorW = calculaVetorW(vetorE)
-
-# O vetor T terá os mesmos valores do vetor W, 
-# com a diferença que o menor elemento (valor absoluto) de T será setado para 1
-vetorT = calculaVetorT(vetorW)
-
-# Vetor U será o vetor unitário do produto vetorial entre os vetores T e W
-vetorU = calculaVetorU(vetorT, vetorW)
-
-# Vetor V será o resultado do produto vetorial entre os vetores W e U
-vetorV = calculaVetorV(vetorW, vetorU)
-
-# print(vetorU)
-# print(vetorV)
-# print(vetorW)
-
-matrizImagem = calculaValoresUV(matrizImagem, numColunas, numlinhas, left, right, top, bottom)
-
-# casoOrtografico(matrizImagem, numColunas, numlinhas, vetorE, vetorU, vetorV, vetorW)
-
-casoObliquo(matrizImagem, numColunas, numlinhas, vetorE, vetorU, vetorV, vetorW, distancia)
-
-# Caso Oblíquo e Ortogŕaficos concluídos
-
-""" 
-    Próximo passo é fazer o cálculo das funções implícitas
-    Para isso, precisamos calcular a variável t na função e + td 
-    onde e = origem, d = direção. 
-    Nesse caso, vamos explorar a interseção entre raio X esfera
-"""
-
-# Vetor que indica a posição do centro da esfera e seu respectivo raio
-centroEsfera = [0, 0, 0]
-raioEsfera = 0
-
-""" 
-    Para obter o valor de t, primeiro precisamos saber se raio toca ou não a esfera.
-    Primeiro calcula-se o delta, que é dado pela expressão abaixo:
-    ∆ = (2d · (e − c))**2 − 4(d · d)((e − c) · (e − c) − r**2 )
-
-    Se ∆ < 0, o raio não intercepta a esfera.
-    Se ∆ = 0, o raio toca exatamente um ponto na casca da esfera
-    Se ∆ > 0, existem duas soluções:
-    O raio entra na esfera
-    O raio sai da esfera
-"""
-
-def calculoDelta(matriz, nX, nY, centro, raio):
-    matrixResultante = matriz[:]
-    for i in range(nX):
-        for j in range(nY):
-            origem  = matrixResultante[i][j][0]
-            direcao = matrixResultante[i][j][1]
-
+def Calculo_Delta(matriz_caso, colunas, linhas, centro, raio):
+    matriz_result = matriz_caso[:]
+    for x in range(colunas):
+        for y in range(linhas):
+            direcao = matriz_caso[x][y][0]
+            origem  = matriz_caso[x][y][1]
             a = numpy.dot(direcao, direcao)
-
-            b = 2 * numpy.dot(direcao,(origem - centro))
-
-            c = numpy.dot((origem - centro), (origem - centro)) - raio**2
             
-            # Cálculo Delta
-            delta = b**2 - (4 * a *c)
 
-            # Se o delta for maior que 0, o raio pode entrar ou sair da esfera
             if(delta >= 0):
-                valorT1 = abs(- numpy.dot(direção, (origem - centro)) + sqrt(delta))
-                valorT2 = abs(- numpy.dot(direção, (origem - centro)) - sqrt(delta))
-                if (valorT1 >= valorT2)
-                    matrixResultante[i][j] = valorT1
-                else:
-                    matrixResultante[i][j] = valorT2
+                matriz_result[x][y] = (100, 150, 100)
+            else:
+                matriz_result[x][y] = (0, 0, 0)
+    return matriz_result
 
-    return matrixResultante
 
-imagemEsfera = calculoDelta(matrizImagem, numColunas, numlinhas, centroEsfera, raioEsfera)
+def Cria_Imagem(matriz_img, colunas, linhas):
+        img = PilImage.new('RGB', (colunas, linhas))
+        imagem = img.load()
+        for x in range(colunas):
+            for y in range(linhas):
+                imagem[x, y] = (matriz_img[x][y][0], 
+                matriz_img[x][y][1],
+                matriz_img[x][y][2])
+        img.show()
+        img.save("resultado.jpg")
+
+# Valores dos lados do plano da imagem
+left      = -13
+right     =  13
+top       =  10
+bottom    = -10
+distancia =  4
+
+# Declaração de vetores que serão usados
+vetor_a = [10, 10, 10]
+vetor_w = [0, 0, 0]
+vetor_t = [0, 0, 0]
+vetor_v = [0, 0, 0]
+vetor_u = [0, 0, 0]
+num_colunas = 640
+num_linhas  = 480
+centro_esfera = [0, 0, 0]
+raio_esfera   = 1
+cor_esfera    = (100, 150, 100)
+
+vetor_w  = Calculo_Vetor_W(vetor_a)
+# print(vetor_w)
+# print(vetor_a)
+vetor_t  = Calculo_Vetor_T(vetor_w)
+# print(vetor_t) 
+vetor_u  = Calculo_Vetor_U(vetor_t, vetor_w)
+# print(vetor_u)
+vetor_v  = Calculo_Vetor_V(vetor_w, vetor_u)
+# print(vetor_v)
+
+matriz_uv = Criar_Matriz(num_colunas, num_linhas, 0)
+
+# print(matriz_uv)
+Calculo_Valore_U_V(matriz_uv, num_colunas, num_linhas, left, right, bottom, top)
+# print(matriz_uv)
+# print(matriz_uv[639][477])
+# print(matriz_uv[639][478])
+# print(matriz_uv[639][479])
+
+# matriz_imagem = Caso_Ortografico(matriz_uv, num_colunas, num_linhas, vetor_a, vetor_w, vetor_u, vetor_v)
+# print(matriz_imagem)
+matriz_caso_obliquo = Caso_Obliquo(matriz_uv, num_colunas, num_linhas, vetor_a, vetor_w, vetor_u, vetor_v, distancia)
+
+matriz_imagem = Calculo_Delta(matriz_caso_obliquo, num_colunas, num_linhas, centro_esfera, raio_esfera)
+
+# print(matriz_imagem)
+Cria_Imagem(matriz_imagem, num_colunas, num_linhas)
+
+
 
